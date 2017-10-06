0019ba6a2ca0f82837c2c119bf6e6ef51feecba0
Prajakta Kinjawadekar <kinjawadekar_p@India.XoriantCorp.com>
2017-10-06 10:20:58 +0530

    Complete with unit Testing

--- shopwise.py
+++ shopwise.py
@@ -2,7 +2,7 @@ import os
 import sqlite3
 import json
 from flask import Flask, request, session, g, redirect, url_for, abort, \
-     render_template, flash, jsonify
+     render_template, flash
 
 
 app = Flask(__name__)
@@ -14,7 +14,6 @@ app.config.update(dict(
 ))
 
 app.config.from_envvar('PROJECT_SETTINGS', silent=True)
-
 
 def connect_db():
     rv = sqlite3.connect(app.config['DATABASE'])
@@ -31,11 +30,15 @@ def init_db():
 
 @app.cli.command('initdb')
 def initdb_command():
+    """Initializes the database."""
     init_db()
     print 'Initialized the database'
 
 
 def get_db():
+    """Opens a new database connection if there is none yet for the
+    current application context.
+    """
    if not hasattr(g, 'sqlite_db'):
      g.sqlite_db = connect_db()
    return g.sqlite_db
@@ -43,36 +46,45 @@ def get_db():
 
 @app.teardown_appcontext
 def close_db(error):
+    """Closes the database again at the end of the request."""
     if hasattr(g, 'sqlite_db'):
       g.sqlite_db.close()
   
   
 @app.route('/')
 def homepage():
+    """Creates the url for the homepage of the application"""
+    flash("Welcome to home Page")
     return render_template('homepage.html')
  
  
 @app.route('/register', methods=['GET', 'POST'])
 def register():
+    """Creates the url for registration of new User"""
     firstname = request.form['firstname']
     lastname = request.form['lastname']
     username = request.form['username']
     password = request.form['password']
     db = get_db()
     cur = db.cursor()
-    db.execute('insert into registered_user(firstname, lastname, username, password) values(?, ?, ?, ?)',[firstname, lastname, username, password])
+    db.execute('insert into registered_user(firstname, lastname,\
+        username, password) values(?, ?, ?, ?)',\
+		[firstname, lastname, username, password]\
+	)
     db.commit()
-    print "Registered!!"
     return render_template('homepage.html')
  
  
 @app.route('/login', methods=['GET', 'POST'])
 def login():
+    """Creates the url for login of registered User"""
     username = request.form['username']
     password = request.form['password']
     db = get_db()
-    cur = db.execute('select * from registered_user where username = \
-    (?) and password = (?)', [username, password])  
+	#To check whether user is a registered user
+    cur = db.execute('select * from registered_user where \
+	    username = (?) and password = (?)', [username, password]\
+	)  
     data = list( cur.fetchone())
     if data is not None:
         session['username'] = data[3]
@@ -80,48 +92,76 @@ def login():
         session['lastname'] = data[2]
         session['id'] = data[0]
         rows = showad()
-        return render_template('userHomePage.html', firstname=data[1], lastname=data[2], rows=rows)
+        return render_template('userHomePage.html',\
+		    firstname=data[1], lastname=data[2], rows=rows\
+		)
     return ""
    
    
 @app.route('/postAd',methods=['GET', 'POST'])
 def postAd():
+    """Creates the url for posting ad by the logged in User"""
     if request.method == 'POST':
         db = get_db()
         cur = db.cursor()
-        cur = db.execute('insert into advertisements(title, category, price, name, mobile, email, address, user_id) values(?, ?, ?, ?, ?, ?, ?, ?)',[request.form['title'], request.form['category'], request.form['price'], request.form['name'], request.form['mobile'], request.form['email'], request.form['address'], session['id']])
+        cur = db.execute('insert into advertisements(\
+		    title, category, price, name, mobile, email,\
+			address, user_id) values(?, ?, ?, ?, ?, ?, ?, ?)',\
+			[request.form['title'], request.form['category'],\
+			request.form['price'], request.form['name'],\
+			request.form['mobile'], request.form['email'],\
+			request.form['address'], session['id']]\
+		)
         db.commit()
         rows=showad()
-        return render_template('userHomePage.html',firstname=session['firstname'], lastname=session['lastname'],rows=rows)    
+        return render_template('userHomePage.html',\
+		    firstname=session['firstname'],\
+			lastname=session['lastname'],rows=rows\
+		)    
     return render_template('postAd.html')
     
 
 def showad():
+    """To retrieve all advertisements by the current logged in User"""
     db = get_db()
-    cur = db.execute('select id,title,category,price from advertisements where user_id =(?)',[session['id']])
+    cur = db.execute('select id,title,category,price\
+	    from advertisements where user_id =(?)',[session['id']]\
+	)
     rows = cur.fetchall()
-    print "Inside"
     return rows
 
 
 @app.route('/editad', methods=['GET', 'POST'])    
 @app.route('/editad/<int:ad_id>', methods=['GET', 'POST'])
 def editad(ad_id=None):
+    """Creates the url for editing advertisement created 
+	by the logged in user
+	"""
     if request.method == 'POST': 
         db = get_db()
         cur = db.cursor()
-        cur = db.execute('update advertisements set title = (?), category = (?), price = (?),\
-        name = (?), mobile = (?), email = (?), address = (?), user_id = (?) where id = (?)',\
-        [request.form['title'], request.form['category'], request.form['price'],request.form['name'], request.form['mobile'], request.form['email'],\
-        request.form['address'], session['id'], session['ad_id']])
+        cur = db.execute('update advertisements set title = (?),\
+  		    category = (?), price = (?),\
+            name = (?), mobile = (?), email = (?),\
+			address = (?), user_id = (?) where id = (?)',\
+            [request.form['title'], request.form['category'],\
+		    request.form['price'],request.form['name'],\
+			request.form['mobile'], request.form['email'],\
+            request.form['address'], session['id'], session['ad_id']]\
+		)
         db.commit()
         print session['ad_id']
-        return render_template('userHomePage.html', firstname=session['firstname'], lastname=session['lastname'], rows=showad())
+        return render_template('userHomePage.html',\
+ 		    firstname=session['firstname'],\
+			lastname=session['lastname'], rows=showad()\
+		)
     else:
         db = get_db()
         print ad_id    
         session['ad_id'] = ad_id
-        cur = db.execute('select * from advertisements where id = (?)', [session['ad_id']])
+        cur = db.execute('select * from advertisements\
+ 		    where id = (?)', [session['ad_id']]\
+		)
         ad_data = cur.fetchone()
     return render_template('postAd.html', ad_data=ad_data)     
 
@@ -129,12 +169,16 @@ def editad(ad_id=None):
 @app.route('/delete', methods=['GET'])    
 @app.route('/delete/<int:ad_id>', methods=['GET'])
 def deleteAd(ad_id=None):
+    """Creates the url for deleting ad by the logged in user"""
     db = get_db()
     cur = db.cursor()
     db.execute('delete from advertisements where id = (?)', [ad_id])
     db.commit()
     flash("Deleted Successfully!!!! ")
-    return render_template('userHomePage.html', firstname=session['firstname'], lastname=session['lastname'], row=showad())
+    return render_template('userHomePage.html',\
+ 	    firstname=session['firstname'],\
+		lastname=session['lastname'], row=showad()\
+	)
     
     
 if __name__ == '__main__':    
---
+++ testing.py
@@ -0,0 +1,87 @@
+
+import os
+import shopwise
+import unittest
+import tempfile
+from flask import Flask
+
+
+class ShopWiseTestCase(unittest.TestCase):
+
+
+    @classmethod
+    def setUpClass(cls):
+	    ''' To create mock database connection '''
+	    cls.db_fd, shopwise.app.config['DATABASE'] = tempfile.mkstemp()
+	    shopwise.app.testing = True
+	    cls.app = shopwise.app.test_client()
+	    with shopwise.app.app_context():
+	        shopwise.init_db()
+			
+			
+    @classmethod	
+    def tearDownClass(cls):
+	    ''' To close the database connection '''
+        os.close(cls.db_fd)
+        os.unlink(shopwise.app.config['DATABASE'])
+	
+	
+    def test_homepage(self):
+	    ''' Test case to check homepage '''
+	    rv = self.app.get('/')
+	    self.assertEqual(rv.status_code, 200)
+
+
+    def register(self, firstname, lastname, username, password):   
+        return self.app.post('/register', data=dict(
+		    firstname = firstname,
+			lastname = lastname,
+            username=username,
+            password=password
+        ), follow_redirects=True)
+	
+	
+    def test_register(self):
+	    ''' Test case to check registration of new User '''
+        rv = self.register('Aarti', 'Walimbe', 'aa', 'aa')
+	    self.assertEqual(rv.status_code, 200)
+	
+	
+    def login(self, username, password):
+        return self.app.post('/login', data=dict(
+            username=username,
+            password=password
+        ), follow_redirects=True)
+	
+	
+    def test_login(self):
+	    ''' Test case to check successful login of User '''
+	    rv = self.register('Prajakta', 'kinjawadekar', 'aa', 'aa')
+	    rv1 = self.login('aa', 'aa')
+	    self.assertEqual(rv1.status_code, 200)
+	
+	
+    def postAd(self, title, category, price, name, mobile, email, address):
+        return self.app.post('/postAd', data=dict(
+            title = title,
+            category = category, 
+            price = price,
+		    name = name,
+		    mobile = mobile,
+		    email = email,
+		    address = address    
+        ), follow_redirects=True)
+	
+	
+    def test_postAd(self):
+	    ''' Test case to check successful posting of ad by User '''
+	    rv = self.register('Aarti', 'Walimbe', 'aa', 'aa')
+	    rv = self.login('aa', 'aa')
+	    rv1 = self.postAd('aa', 'Cars', 67, 'gfygdu', 89578598, 'fuis@gmail', 'gr' )
+	    self.assertEqual(rv1.status_code, 200)
+	
+	
+if __name__ == '__main__':
+    unittest.main()
+  
+    
